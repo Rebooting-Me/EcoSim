38949b573e1e95cf74d24d240b637d07d61ec131
Rebooting-Me <arsharma@ucsd.edu>
2023-03-18 16:40:59 -0700

    minor

--- firstGen.js
+++ firstGen.js
@@ -19,6 +19,6 @@ function createFirstGen(size) {
             genome: { keys: [key1, key2, key3, key4] }
         };
     }
-    return organism[0].genome.keys;
+    return organism;
 }
 exports.createFirstGen = createFirstGen;
