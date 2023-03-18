29c458de181ae21de5a613ad5d0f4fefb1da47ca
Rebooting-Me <arsharma@ucsd.edu>
2023-03-18 16:32:56 -0700

    nodemon

--- firstGen.ts
+++ firstGen.ts
@@ -20,7 +20,7 @@ function createFirstGen (size: number) {
             genome : {keys: [key1, key2, key3, key4]}
         }
     }
-    return organism[0].genome.keys;
+    return organism;
 }
 
 export { createFirstGen }
