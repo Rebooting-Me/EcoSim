40997d17f3659597424312b76388bc3cf206a3e6
Rebooting-Me <arsharma@ucsd.edu>
2023-03-18 15:52:18 -0700

    Some documentation

--- firstGen.ts
+++ firstGen.ts
@@ -17,10 +17,10 @@ function createFirstGen (size: number) {
                 hunger: genHunger,
                 direction: directions[genDir]
             },
-            genome : {key: [key1, key2, key3, key4]}
+            genome : {keys: [key1, key2, key3, key4]}
         }
     }
-    return organism[0].genome.key;
+    return organism[0].genome.keys;
 }
 
 export { createFirstGen }
